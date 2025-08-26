b65a53746c3af03c563c59626942e77e9e7a1531
Orinea Tovhakale <orineatovhakale@Orineas-MacBook-Air.local>
2025-08-26 16:42:16 +0200

    Update Landing page with new adjustment

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 
 const Landing = () => (
   <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
     <h1 className="text-2xl mb-4">Job Application Tracker</h1>
-    <Link to="/register" className="text-blue-500 hover:underline mb-4">Register</Link>
-    <Link to="/login" className="text-blue-500 hover:underline">Login</Link>
+    <Link to="/register" className="text-blue-500 hover:underline mb-4">
+      <Button>Register</Button>
+    </Link>
+    <br></br>
+    <Link to="/login" className="text-blue-500 hover:underline">
+      <Button variant="edit" >Login</Button>
+    </Link>
   </div>
 );
 
